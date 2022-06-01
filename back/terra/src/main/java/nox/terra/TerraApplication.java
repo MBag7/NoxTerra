ea58ba6e8a630507f1f4d251d45eaafcc9daba50
MBag7 <mbagnicki7@gmail.com>
2022-06-01 21:52:40 +0200

    Initial Commit of NoxTerra

---
+++ back/terra/src/main/java/nox/terra/TerraApplication.java
@@ -0,0 +1,13 @@
+package nox.terra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TerraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TerraApplication.class, args);
+	}
+
+}
---
+++ back/terra/src/test/java/nox/terra/TerraApplicationTests.java
@@ -0,0 +1,13 @@
+package nox.terra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TerraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
